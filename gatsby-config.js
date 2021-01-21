af8f10ee115ce97d604d94b0996be88ab996b38c
almamla <36993805+almamla@users.noreply.github.com>
2021-01-21 15:13:14 +0000

    updated content, added GA tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,14 +17,14 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-netlify`,
-    // {
-    //   resolve: `gatsby-plugin-gtag`,
-    //   options: {
-    //     trackingId: `UA-XXXXXXXX-X`,
-    //     head: false,
-    //     anonymize: true,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        trackingId: `UA-187853920-1`,
+        head: false,
+        anonymize: true,
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
